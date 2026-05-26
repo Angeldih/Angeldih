ec1ba0d8f5efea62a3e8b3d348d0234215e1398a
Angeldih <leon.thomas@student.otahuhucollege.school.nz>
2026-05-26 12:13:32 +1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function loadPage(page){
+
+    fetch(page)
+    .then(response=> response.text())
+    .then(data => {
+
+        document.getElementById("content").innerHTML = data;
+        
+    })
+}
